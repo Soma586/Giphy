646866153f2218a443ea3ae34bb581bbb4299d75
Tarik <lightspeedkey@yahoo.com>
2019-06-10 09:48:50 -0400

    Updates

---
+++ precache-manifest.36c5eed429bb624db6d463f6722ba21c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646c2e217309e623f2a2b867bde57382",
+    "url": "/Giphy/./index.html"
+  },
+  {
+    "revision": "cd62ee8dadbab24ac8d9",
+    "url": "/Giphy/./static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "92b0b7bcc3aa3847c2d7",
+    "url": "/Giphy/./static/js/2.69181713.chunk.js"
+  },
+  {
+    "revision": "cd62ee8dadbab24ac8d9",
+    "url": "/Giphy/./static/js/main.5f1937c5.chunk.js"
+  },
+  {
+    "revision": "fe263a00c44d2c08d4c9",
+    "url": "/Giphy/./static/js/runtime~main.46eb41e5.js"
+  }
+]);
