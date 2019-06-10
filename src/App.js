a13b2b43ea202b2f1bea2951b4b9c997b4e283a5
Sabo <lightspeedkey@yahoo.com>
2019-06-09 23:46:48 -0400

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import Results from './Results';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: this.props, imageurl: [] }; //Tag for search
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.search = this.search.bind(this);
+    }
+
+    //For when input is being placed
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    //For when search field is empty 
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    search = () => {
+        axios.get('http://api.giphy.com/v1/gifs/search?q='+ this.state.value +'&api_key=JQI7xYodCQUL8IIV4Iddwf7Sigw1Snl6')
+            .then((response) => {
+                console.log(response)
+                this.setState({
+                    imageurl: response.data.data
+                });
+            })
+            .catch(function (error) {
+                //Error Text
+            })
+    }
+
+    render() {
+        const images = this.state.imageurl.map( (i) =><Results i={i}/> )
+        return (
+            <div>
+                <h2>Search for a gif:</h2>
+                <input type="search" placeholder="Search" onChange={this.handleChange}></input>
+                <input type="submit" onClick={this.search}></input>
+                <br></br>
+                {images}
+            </div>
+        )
+    }
 }
 
 export default App;
